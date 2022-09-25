3bdaeec5f297f5a0e0638a813acc9abd35ff5566
pweingar <pweingar@gmail.com>
2022-09-25 09:02:55 -0400

    Memory Maps
    Incorporated memory maps

--- tex/csvtable.py
+++ tex/csvtable.py
@@ -1,17 +1,19 @@
 
 import csv
 
-with open('io_page_1_tab.tex', 'w') as outfile:
+with open('io_page_0_tab.tex', 'w') as outfile:
     outfile.write("\\begin{table}\n")
-    outfile.write("\\begin{tabular}{|c|l|} \\hline\n")
+    outfile.write("    \\begin{center}\n")
+    outfile.write("        \\begin{tabular}{|c|c|l|} \\hline\n")
+    outfile.write("            Start & End & Purpose \\\\ \\hline \\hline\n")
 
-    with open('io_page_1.csv') as csvfile:
+    with open('io_page_0.csv') as csvfile:
         io_blocks = csv.reader(csvfile)
         for device in io_blocks:
-            outfile.write("\\verb+{0}+ & \multirows{{2}}{{*}}{{{1}}} \\\\ \\cline{{1-1}}\n".format(device[0], device[2]))
-            outfile.write("\\verb+{0}+ & \\\\ \\hline\n".format(device[1]))
+            outfile.write("            \\verb+{0}+ & \\verb+{1}+ & {2} \\\\ \\hline\n".format(device[0], device[1], device[2]))
 
-    outfile.write("\\caption{I/O Page 1 Addresses}\n")
-    outfile.write("\\label{tab:io_page_1}\n")
-    outfile.write("\\end{tabular}\n")
+    outfile.write("        \\end{tabular}\n")
+    outfile.write("        \\caption{I/O Page 0 Addresses}\n")
+    outfile.write("        \\label{tab:io_page_0}\n")
+    outfile.write("    \\end{center}\n")
     outfile.write("\\end{table}\n")
